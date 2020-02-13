dc4be07079e975a0ba4f2bd5fcd495c060fb8d70
Martin Finkel <martin@videolabs.io>
2020-02-13 15:19:27 +0700

    Dont compile d3d11 code

--- Assets/PluginSource/RenderAPI.cpp
+++ Assets/PluginSource/RenderAPI.cpp
@@ -5,11 +5,14 @@
 
 RenderAPI* CreateRenderAPI(UnityGfxRenderer apiType)
 {
+#	if SUPPORT_D3D11
     if (apiType == kUnityGfxRendererD3D11)
     {
         extern RenderAPI* CreateRenderAPI_D3D11();
         return CreateRenderAPI_D3D11();
     }
+#	endif // if SUPPORT_D3D11
+
 #	if SUPPORT_OPENGL_UNIFIED
 	if (apiType == kUnityGfxRendererOpenGLCore || apiType == kUnityGfxRendererOpenGLES20 || apiType == kUnityGfxRendererOpenGLES30)
 	{
