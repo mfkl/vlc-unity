a4d3c0c950f8b44125805af4a1cc8d2740709097
Martin Finkel <martin@videolabs.io>
2020-02-20 17:11:39 +0700

    Call JNI_OnLoad back on libvlcjni

--- Assets/PluginSource/RenderAPI_Android.cpp
+++ Assets/PluginSource/RenderAPI_Android.cpp
@@ -25,10 +25,10 @@ jint JNI_OnLoad(JavaVM* vm, void* reserved)
     //     return -1;
     // }
 
-    handle = dlopen("libvlc.so", RTLD_LAZY);
+    handle = dlopen("libvlcjni.so", RTLD_LAZY);
     if (!handle)
     {
-        DEBUG("could not link libvlc.so");
+        DEBUG("could not link libvlcjni.so");
         return -1;
     }
 
