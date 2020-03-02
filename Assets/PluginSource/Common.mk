b7d2b85e0b6ac2fd0bb5b9062b83c0f8d1423db7
Martin Finkel <martin@videolabs.io>
2020-03-02 12:14:36 +0700

    build: revert common.mk changes
    mk android files are in /jni

--- Assets/PluginSource/Common.mk
+++ Assets/PluginSource/Common.mk
@@ -1,46 +1,31 @@
 TARGET = VLCUnityPlugin
 
-ifeq ($(PLATFORM), win)
 SRCS = RenderingPlugin.cpp RenderAPI.cpp RenderAPI_D3D11.cpp Log.cpp
-else
-SRCS = RenderingPlugin.cpp RenderAPI.cpp RenderAPI_Android.cpp RenderAPI_OpenGLBase.cpp RenderAPI_OpenGLEGL.cpp Log.cpp
-endif
 
 OBJS = $(SRCS:.cpp=.o)
 
-CXXFLAGS = -O2 -fPIC -fdebug-prefix-map='/mnt/c/'='c:/' -Wall -I./include/ -I/usr/lib/jvm/default-java/include/ -I/usr/lib/jvm/default-java/include/linux/
+CXXFLAGS = -O2 -fdebug-prefix-map='/mnt/c/'='c:/' -Wall -I./include/
 
-ifeq ($(PLATFORM), win)
+ifeq ($(ARCH), x86_64)
 LIB=/mnt/d/vlc-4.0.0-dev/sdk/lib
 else
-LIB=/mnt/c/Users/Martin/Projects/vlc-unity
+LIB=/mnt/d/vlc-4.0.0-dev-x86/sdk/lib
 endif
-
-
-ifeq ($(PLATFORM), win)
 LDFLAGS = -static-libgcc -static-libstdc++ -shared -Wl,-pdb= -L$(LIB)
-else
-LDFLAGS = -static-libgcc -static-libstdc++ -shared -L$(LIB)
-endif
 
-ifeq ($(PLATFORM), win)
 LIBS = -lvlc -ld3d11 -ld3dcompiler_47 -ldxgi
-else
-LIBS = -lvlc -lGLESv2 
-endif
 
-ifeq ($(PLATFORM), win)
+ifeq ($(ARCH), x86_64)
 BIN_PREFIX = x86_64-w64-mingw32
-COMPILEFLAG = -m64
-endif
-
-ifeq ($(PLATFORM), win)
-OUTPUT = $(TARGET).dll
+COMPILEFLAG = m64
 else
-OUTPUT = $(TARGET).so
+BIN_PREFIX = i686-w64-mingw32
+COMPILEFLAG = m32
 endif
 
-CXX = g++ -DUNITY_ANDROID=1
+OUTPUT = $(TARGET).dll
+
+CXX = $(BIN_PREFIX)-c++
 STRIP = $(BIN_PREFIX)-strip
 
 all: $(OUTPUT)
@@ -58,4 +43,4 @@ $(OUTPUT): $(OBJS)
 	$(CXX) $(LDFLAGS) -o $(OUTPUT) $(OBJS) $(LIBS)
 
 .cpp.o:
-	$(CXX) $(CXXFLAGS) $(COMPILEFLAG) -c -o $@ $<
+	$(CXX) $(CXXFLAGS) -$(COMPILEFLAG) -c -o $@ $<
